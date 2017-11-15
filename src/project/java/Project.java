25e9cf325cda449dadfd8c265070189cdc29a433
Teletha <Teletha.T@gmail.com>
2017-11-15 13:16:33 +0900

    Initial commit.

---
+++ src/project/java/Project.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Viewtify Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+public class Project extends bee.api.Project {
+
+    {
+        product("npc", "viewtify", "1.0");
+
+        require("com.github.teletha", "sinobu", "1.0");
+        require("org.controlsfx", "controlsfx", "8.40.14");
+    }
+}
