5a152a060a5ecbcb00cf7b05d323ec98360dfa70
Teletha <Teletha.T@gmail.com>
2017-12-28 08:49:04 +0900

    Progress...

--- src/main/java/viewtify/ui/UITab.java
+++ src/main/java/viewtify/ui/UITab.java
@@ -13,19 +13,29 @@ import java.util.Objects;
 
 import javafx.scene.control.Tab;
 
+import viewtify.ui.helper.StyleHelper;
+
 /**
  * @version 2017/12/27 15:55:39
  */
-public class UITab {
+public class UITab implements StyleHelper<UITab, Tab> {
 
     /** The actual ui. */
-    private final Tab ui;
+    public final Tab ui;
 
     /**
      * @param tab
      */
     public UITab(Tab tab) {
         this.ui = tab;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Tab ui() {
+        return ui;
     }
 
     /**
