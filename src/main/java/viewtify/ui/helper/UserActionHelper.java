d47a383a5caf370b03461a2217898bfeecaddaa6
Teletha <Teletha.T@gmail.com>
2019-04-06 17:44:41 +0900

    Update sinobu.

--- src/main/java/viewtify/ui/helper/UserActionHelper.java
+++ src/main/java/viewtify/ui/helper/UserActionHelper.java
@@ -114,7 +114,7 @@ public interface UserActionHelper<Self extends UserActionHelper> {
      * @return Chainable API.
      */
     private <E extends Event> Self when(Signal<User<E>> actionTypes, WiseRunnable listener) {
-        return when(actionTypes, (WiseConsumer<E>) listener.postfix());
+        return when(actionTypes, I.wiseC(listener));
     }
 
     /**
