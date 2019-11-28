dc052066b2a0bafa434cd5c0e0b98d40af33b9b2
Teletha <Teletha.T@gmail.com>
2019-11-28 17:25:03 +0900

    Add various property types.

--- src/main/java/viewtify/ui/helper/PropertyHelper.java
+++ src/main/java/viewtify/ui/helper/PropertyHelper.java
@@ -10,8 +10,16 @@
 package viewtify.ui.helper;
 
 import javafx.beans.property.Property;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.OverrunStyle;
+import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 import kiss.I;
 
@@ -43,6 +51,23 @@ public interface PropertyHelper {
      * 
      */
     class Type<T> {
+        /** The defined property type. */
+        public static final Type<Pos> Alignment = new Type("alignment");
+
+        /** The defined property type. */
+        public static final Type<ContentDisplay> ContentDisplay = new Type("contentDisplay");
+
+        /** The defined property type. */
+        public static final Type<ContextMenu> ContextMenu = new Type("contextMenu");
+
+        /** The defined property type. */
+        public static final Type<Cursor> Cursor = new Type("cursor");
+
+        /** The defined property type. */
+        public static final Type<Boolean> Disable = new Type("disable");
+
+        /** The defined property type. */
+        public static final Type<String> EllipsisString = new Type("ellipsisString");
 
         /** The defined property type. */
         public static final Type<Font> Font = new Type("font");
@@ -51,7 +76,43 @@ public interface PropertyHelper {
         public static final Type<Node> Graphic = new Type("graphic");
 
         /** The defined property type. */
+        public static final Type<Double> GraphicTextGap = new Type("graphicTextGap");
+
+        /** The defined property type. */
+        public static final Type<String> Id = new Type("id");
+
+        /** The defined property type. */
+        public static final Type<Double> LineSpacing = new Type("lineSpacing");
+
+        /** The defined property type. */
+        public static final Type<Boolean> MnemonicParsing = new Type("mnemonicParsing");
+
+        /** The defined property type. */
+        public static final Type<Double> Opacity = new Type("opacity");
+
+        /** The defined property type. */
         public static final Type<String> Text = new Type("text");
+
+        /** The defined property type. */
+        public static final Type<TextAlignment> TextAlignment = new Type("textAlignment");
+
+        /** The defined property type. */
+        public static final Type<Paint> TextFill = new Type("textFill");
+
+        /** The defined property type. */
+        public static final Type<OverrunStyle> TextOverrun = new Type("textOverrun");
+
+        /** The defined property type. */
+        public static final Type<Tooltip> Tooltip = new Type("tooltip");
+
+        /** The defined property type. */
+        public static final Type<Boolean> Underline = new Type("underline");
+
+        /** The defined property type. */
+        public static final Type<Boolean> Visible = new Type("visible");
+
+        /** The defined property type. */
+        public static final Type<Boolean> WrapText = new Type("wrapText");
 
         /** The property name. */
         private final String name;
