9d79799e6679a34e2a552d529ea8912a4b6e7290
Teletha <Teletha.T@gmail.com>
2020-03-17 19:15:30 +0900

    UITab can return its styleable node.

--- src/main/java/viewtify/ui/UITab.java
+++ src/main/java/viewtify/ui/UITab.java
@@ -9,17 +9,35 @@
  */
 package viewtify.ui;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 
+import javafx.scene.Node;
 import javafx.scene.control.Tab;
 
 import kiss.I;
+import kiss.WiseFunction;
 import viewtify.ui.helper.ContextMenuHelper;
 import viewtify.ui.helper.LabelHelper;
 import viewtify.ui.helper.StyleHelper;
 
 public class UITab extends Tab implements StyleHelper<UITab, Tab>, LabelHelper<UITab>, ContextMenuHelper<UITab> {
+
+    /** Cache to find tab node. */
+    private static final WiseFunction<Node, Object> findTab;
+
+    static {
+        try {
+            Method m = Class.forName("javafx.scene.control.skin.TabPaneSkin$TabHeaderSkin").getMethod("getTab");
+            m.setAccessible(true);
+
+            findTab = m::invoke;
+        } catch (Exception e) {
+            throw I.quiet(e);
+        }
+    }
 
     /** The parent view. */
     private final View parent;
@@ -29,6 +47,13 @@ public class UITab extends Tab implements StyleHelper<UITab, Tab>, LabelHelper<U
 
     /** Tab state. */
     private final AtomicBoolean loaded = new AtomicBoolean();
+
+    /** The cached reference for the styleable node . */
+    private WeakReference<Node> styleable;
+
+    public UITab() {
+        this(null);
+    }
 
     /**
      * 
@@ -100,5 +125,25 @@ public class UITab extends Tab implements StyleHelper<UITab, Tab>, LabelHelper<U
             view.initializeLazy(parent);
             setContent(view.ui());
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public synchronized Node getStyleableNode() {
+        if (styleable == null) {
+            for (Node node : getTabPane().lookupAll(".tab")) {
+                if (findTab.apply(node) == this) {
+                    styleable = new WeakReference(node);
+                    break;
+                }
+            }
+
+            if (styleable == null) {
+                return null;
+            }
+        }
+        return styleable.get();
     }
 }
