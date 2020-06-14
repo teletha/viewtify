21a84bed8f49f8ad63dbe09d4ce5027cbfa4df6f
Teletha <Teletha.T@gmail.com>
2020-06-15 03:12:19 +0900

    Update EnhanceCanvas.

--- src/main/java/viewtify/ui/canvas/EnhancedCanvas.java
+++ src/main/java/viewtify/ui/canvas/EnhancedCanvas.java
@@ -9,12 +9,29 @@
  */
 package viewtify.ui.canvas;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+
+import viewtify.util.FXUtils;
 
 public class EnhancedCanvas extends Canvas {
+
+    /**
+     * Bind the canvas size to the parent region.
+     * 
+     * @param parent
+     * @return
+     */
+    public EnhancedCanvas fitOn(Region parent) {
+        widthProperty().bind(parent.widthProperty());
+        heightProperty().bind(parent.heightProperty());
+        return this;
+    }
 
     /**
      * Configure graphic context.
@@ -26,6 +43,64 @@ public class EnhancedCanvas extends Canvas {
         if (context != null) {
             context.accept(getGraphicsContext2D());
         }
+        return this;
+    }
+
+    /**
+     * Clear all drawing figure on this canvas.
+     * 
+     * @return Chainable API.
+     */
+    public EnhancedCanvas clear() {
+        getGraphicsContext2D().clearRect(0, 0, getWidth(), getHeight());
+        return this;
+    }
+
+    /**
+     * Configure stroke style.
+     * 
+     * @param color
+     * @return Chainable API.
+     */
+    public EnhancedCanvas configureStroke(stylist.value.Color color) {
+        return configureStroke(FXUtils.color(color));
+    }
+
+    /**
+     * Configure stroke style.
+     * 
+     * @param color
+     * @return Chainable API.
+     */
+    public EnhancedCanvas configureStroke(Color color) {
+        getGraphicsContext2D().setStroke(color);
+        return this;
+    }
+
+    /**
+     * Stroke text on the specified position.
+     * 
+     * @param text A text to draw.
+     * @param x X position.
+     * @param y Y position.
+     * @return Chainable API.
+     */
+    public EnhancedCanvas strokeText(Object text, double x, double y) {
+        getGraphicsContext2D().strokeText(Objects.toString(text), x, y);
+        return this;
+    }
+
+    /**
+     * Stroke text on the specified position.
+     * 
+     * @param text A text to draw.
+     * @param x X position.
+     * @param y Y position.
+     * @param width A text width.
+     * @return Chainable API.
+     */
+    public EnhancedCanvas strokeText(Object text, double x, double y, double width) {
+        getGraphicsContext2D().strokeText(Objects.toString(text), x, y, width);
         return this;
     }
 }
