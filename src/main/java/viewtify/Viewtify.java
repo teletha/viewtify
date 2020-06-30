e96927d3e83166e77713f8a976c7db2d5ec1d009
Teletha <Teletha.T@gmail.com>
2020-07-01 03:33:32 +0900

    Update sinobu.

--- src/main/java/viewtify/Viewtify.java
+++ src/main/java/viewtify/Viewtify.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -250,7 +249,7 @@ public final class Viewtify {
      */
     public Viewtify language(String language) {
         if (language != null) {
-            Transcript.Lang.set(Locale.forLanguageTag(language));
+            Transcript.Lang.set(language);
         }
         return this;
     }
