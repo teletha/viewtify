69f7e49b8e0150269a85905bcf184fd280827c38
Teletha <Teletha.T@gmail.com>
2020-10-20 01:24:10 +0900

    Provide AnimationAssistant.

--- src/main/java/viewtify/ui/UITableView.java
+++ src/main/java/viewtify/ui/UITableView.java
@@ -17,6 +17,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+
 import viewtify.ui.helper.CollectableHelper;
 import viewtify.ui.helper.ContextMenuHelper;
 import viewtify.ui.helper.PlaceholderHelper;
---
+++ src/main/java/viewtify/ui/helper/AnimationAssistant.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package viewtify.ui.helper;
+
+import javafx.animation.FadeTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+import viewtify.ui.UserInterfaceProvider;
+
+public class AnimationAssistant {
+
+    /**
+     * Apply fade in effect.
+     * 
+     * @param ui A target UI.
+     */
+    public static void fadeIn(UserInterfaceProvider<? extends Node> ui) {
+        fadeIn(ui.ui());
+    }
+
+    /**
+     * Apply fade in effect.
+     * 
+     * @param node A target UI.
+     */
+    public static void fadeIn(Node node) {
+        if (node != null) {
+            FadeTransition fade = new FadeTransition(Duration.millis(666), node);
+            fade.setFromValue(0);
+            fade.setToValue(node.getOpacity());
+            fade.play();
+        }
+    }
+}
