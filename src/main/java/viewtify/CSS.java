dd0c774dd21a44d8e05b03ee15f4c134d28b66d4
Teletha <Teletha.T@gmail.com>
2021-03-18 11:29:54 +0900

    Update javafx.

--- src/main/java/viewtify/CSS.java
+++ src/main/java/viewtify/CSS.java
@@ -31,7 +31,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-
 import kiss.I;
 import kiss.WiseBiConsumer;
 
