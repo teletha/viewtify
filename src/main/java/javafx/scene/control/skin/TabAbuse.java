886ccb9011f6d124b977e219717444f0a0c7b93c
Teletha <Teletha.T@gmail.com>
2022-02-25 11:40:42 +0900

    fix: Dragging dock item is broken.

--- src/main/java/javafx/scene/control/skin/TabAbuse.java
+++ src/main/java/javafx/scene/control/skin/TabAbuse.java
@@ -9,13 +9,10 @@
  */
 package javafx.scene.control.skin;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 
 import javafx.scene.Node;
 import javafx.scene.control.TabPane;
-
 import kiss.I;
 
 public class TabAbuse {
@@ -27,19 +24,19 @@ public class TabAbuse {
      * @param parameterTypes A list of method parameter types.
      * @return A special method invoker.
      */
-    private static final MethodHandle invoker(String className, String methodName, Class... parameterTypes) {
+    private static final Method invoker(String className, String methodName, Class... parameterTypes) {
         try {
             Method method = I.type(className).getDeclaredMethod(methodName, parameterTypes);
             method.setAccessible(true);
-            return MethodHandles.lookup().unreflect(method);
+            return method;
         } catch (Throwable e) {
             throw I.quiet(e);
         }
     }
 
-    private static final MethodHandle scrollOffsetGetter = invoker("javafx.scene.control.skin.TabPaneSkin$TabHeaderArea", "getScrollOffset");
+    private static final Method scrollOffsetGetter = invoker("javafx.scene.control.skin.TabPaneSkin$TabHeaderArea", "getScrollOffset");
 
-    private static final MethodHandle scrollOffsetSetter = invoker("javafx.scene.control.skin.TabPaneSkin$TabHeaderArea", "setScrollOffset", double.class);
+    private static final Method scrollOffsetSetter = invoker("javafx.scene.control.skin.TabPaneSkin$TabHeaderArea", "setScrollOffset", double.class);
 
     /**
      * Get the current scroll offset of the specified {@link TabPane}.
