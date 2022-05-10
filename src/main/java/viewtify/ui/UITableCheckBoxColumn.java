58f741382f9795d96067e39425f62cea70e719f2
Teletha <Teletha.T@gmail.com>
2022-05-10 17:28:23 +0900

    feat: Add UITableCheckBoxColumn.

---
+++ src/main/java/viewtify/ui/UITableCheckBoxColumn.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2021 viewtify Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package viewtify.ui;
+
+import java.util.Collections;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class UITableCheckBoxColumn<RowV> extends UITableColumn<RowV, RowV> {
+
+    private final ObservableList<RowV> selected = FXCollections.observableArrayList();
+
+    /**
+     * Enchanced view.
+     * 
+     * @param view A {@link View} to which the widget belongs.
+     */
+    public UITableCheckBoxColumn(View view, Class<RowV> rowType) {
+        super(view, rowType, rowType);
+
+        renderAsCheckBox((ui, value, disposer) -> {
+            ui.observing().to(on -> {
+                if (on) {
+                    selected.add(value.ⅰ);
+                } else {
+                    selected.remove(value.ⅰ);
+                }
+            }, disposer.add(() -> {
+                selected.remove(value.ⅰ);
+            }));
+        });
+    }
+
+    /**
+     * Retrieve the selected items.
+     * 
+     * @return
+     */
+    public List<RowV> selected() {
+        return Collections.unmodifiableList(selected);
+    }
+}
