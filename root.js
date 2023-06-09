3a330b5bd993083eca45d136c69230817898f3ca
teletha <teletha@users.noreply.github.com>
2023-06-09 22:17:42 +0000

    deploy: 325ef298ed7faa241ce5bd776a7ad88c6bd8b95c

--- root.js
+++ root.js
@@ -970,13 +970,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "ViewtySetting",
-			"packageName": "viewtify",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "VisibleHelper",
 			"packageName": "viewtify.ui.helper",
 			"type": "Functional"
